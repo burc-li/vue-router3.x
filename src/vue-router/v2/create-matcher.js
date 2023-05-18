f21656a01232633865eb54402957e082eabb1e24
BurCha <1477996221@qq.com>
2023-05-18 21:17:24 +0800

    feat: 实现addRoutes、addRoute、match方法

---
+++ src/vue-router/v2/create-matcher.js
@@ -0,0 +1,24 @@
+import createRouteMap from './create-route-map'
+
+export default function createMatcher (routes) {
+  const { pathMap } = createRouteMap(routes)
+
+  // 动态添加多个路由规则 在v4.x中已废弃：使用 router.addRoute() 代替
+  function addRoutes (routes) {
+    createRouteMap(routes, pathMap)
+  }
+  // 动态添加一条新路由规则
+  function addRoute (route) {
+    createRouteMap([route], pathMap)
+  }
+  // 根据一个路径获取对应的路由信息 在v4.x中已废弃：删除 router.match 改为 router.resolve
+  function match (location) {
+    return pathMap[location]
+  }
+
+  return {
+    addRoutes,
+    addRoute,
+    match
+  }
+}
