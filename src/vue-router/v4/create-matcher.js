c34ade703be5efe491a6a213d8edf0ebc8d181ee
BurCha <1477996221@qq.com>
2023-06-09 09:45:01 +0800

    fix: createRouteMap中使用pathList 存储所有的 path

--- src/vue-router/v4/create-matcher.js
+++ src/vue-router/v4/create-matcher.js
@@ -6,15 +6,19 @@ import createRouteMap from './create-route-map'
  * @todo 2. 实现 addRoutes、addRoute、match方法
  */
 export default function createMatcher (routes) {
-  const { pathMap } = createRouteMap(routes)
+  // pathList：收集所有的路由路径，['/', '/a', '/b', '/about', '/about/a', '/about/b']
+  // pathMap：收集路径的对应路由记录，['/':{/的记录}, '/a':{/a的记录}, '/b':{/b的记录}, '/about':{/about的记录}, ...]
+  const { pathList, pathMap } = createRouteMap(routes)
+
+  console.log('pathList, pathMap', pathList, pathMap)
 
   // 动态添加多个路由规则 在v4.x中已废弃：使用 router.addRoute() 代替
   function addRoutes (routes) {
-    createRouteMap(routes, pathMap)
+    createRouteMap(routes, pathList, pathMap)
   }
   // 动态添加一条新路由规则
   function addRoute (route) {
-    createRouteMap([route], pathMap)
+    createRouteMap([route], pathList, pathMap)
   }
   // 根据一个路径获取对应的路由信息 在v4.x中已废弃：删除 router.match 改为 router.resolve
   function match (location) {
--- src/vue-router/v4/create-route-map.js
+++ src/vue-router/v4/create-route-map.js
@@ -1,20 +1,25 @@
 /**
  * @name 路由映射
- * @todo 根据用户传入的 routes选项，生成扁平化的路由信息
+ * @todo 创建一个路由映射表，根据用户传入的 routes选项，生成扁平化的路由映射表
  */
-export default function createRouteMap (routes, pathMap) {
+export default function createRouteMap (routes, pathList, pathMap) {
+  // 当第一次加载的时候没有 pathList 和 pathMap
+  pathList = pathList || []
   pathMap = pathMap || {}
   routes.forEach(route => {
-    addRouteRecord(route, pathMap)
+    addRouteRecord(route, pathList, pathMap)
   })
 
   return {
+    pathList,
     pathMap
   }
 }
 
 // 添加路由信息
-function addRouteRecord (route, pathMap, parentRecord) {
+// pathList：收集所有的路由路径，['/', '/a', '/b', '/about', '/about/a', '/about/b']
+// pathMap：收集路径的对应路由记录，['/':{/的记录}, '/a':{/a的记录}, '/b':{/b的记录}, '/about':{/about的记录}, ...]
+function addRouteRecord (route, pathList, pathMap, parentRecord) {
   const path = parentRecord ? `${parentRecord.path}${parentRecord.path.endsWith('/') ? '' : '/'}${route.path}` : route.path
   const record = {
     path,
@@ -26,10 +31,11 @@ function addRouteRecord (route, pathMap, parentRecord) {
 
   // 维护路径对应的属性
   if (!pathMap[path]) {
+    pathList.push(path)
     pathMap[path] = record
   }
 
   route.children && route.children.forEach(childRoute => {
-    addRouteRecord(childRoute, pathMap, record)
+    addRouteRecord(childRoute, pathList, pathMap, record)
   })
 }
